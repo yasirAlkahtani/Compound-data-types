050f1eff919a910ef8a2fbd94ac1f564774272f9
yasirAlkahtani <52216321+yasirAlkahtani@users.noreply.github.com>
2019-06-30 20:45:08 +0200

    Update dynamicMemory.cpp

--- dynamicMemory.cpp
+++ dynamicMemory.cpp
@@ -1,14 +1,22 @@
+/*
+FileName: dynamicMemory.cpp
+Created by Muhammad Yasir
+Date: 30.06.2019
+dynamic memory test
+*/
 #include <iostream>
-using namespace std;
 
 int main () {
-   double* pvalue  = NULL; // Pointer initialized with null
-   pvalue  = new double[3];   // Request memory for the variable
 
-   pvalue [1]= 29494.99;     // Store value at allocated address
-   cout << "Value of pvalue : " << pvalue[0] <<" "<< pvalue[1]<<" "<<pvalue[2]<<endl;
+   double* arr; // Pointer initialized with null
+   arr  = new double[3];   // Request memory for the variable
 
-    delete pvalue;// free up the memory.
+   arr [0]= 25;     // Store value at allocated address
+   arr [1]= 11;
+   arr [2]= 46;
+   std::cout << "Value of pvalue : " << arr[0] <<" "<< arr[1]<<" "<<arr[2]<<std::endl;
+
+    delete [] arr;// free up the memory.
 
    return 0;
 }
