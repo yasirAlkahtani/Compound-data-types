e99955ef250e7b854d3b9e090fbeee7fc116a0e4
yasirAlkahtani <52216321+yasirAlkahtani@users.noreply.github.com>
2019-06-30 19:26:24 +0200

    Update CharToInt.cpp

--- CharToInt.cpp
+++ CharToInt.cpp
@@ -1,10 +1,22 @@
+/*
+created by Muhammad Yasir
+Namefile: CharToInt.cpp
+parse char to int & int to char , usefull of auot type
+Date: 30.06.2019
+*/
 #include<iostream>
 
-
 int main(void){
-    // Deklaration
-    char ch = 'A';
+
+    char ch = 'A';//char Declaration
 
     std::cout<<ch<<" : "<<(int)ch<<std::endl;
+
+    (int)ch++;
+
+    auto _ch = ch; //auto Declaration
+    auto _chInt = (int)ch;
+
+    std::cout<<_ch<<" : "<<_chInt<<std::endl;
 
 }
